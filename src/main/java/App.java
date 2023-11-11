47eb17f397e9f9f54938cb7b68166e63b907cd40
Nikolai <kolyamn@gmail.com>
2023-11-11 18:26:55 +0300

    added cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        System.out.println("bean = bean2 ? " + (bean == bean2));
+
+        Cat catBean = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("catBean = catBean2 ? " + (catBean2 == catBean));
     }
 }
